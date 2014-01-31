e0cafaba7ac9b6c70fbd4815ead18a7fd163edde
killdash9 <killdash9>
2014-01-30 19:04:37 -0700

    osx support

---
+++ sys/osx/setup.sh
@@ -0,0 +1,7 @@
+cd ../unix
+sh setup.sh
+cd ../..
+patch -p1 < sys/osx/osx.patch
+echo Configuring game permissions to current user
+sed -i.orig "s,GAMEUID  = games,GAMEUID  = `whoami`," Makefile
+sed -i.orig "s,GAMEGRP  = bin,GAMEGRP  = `id -gn`," Makefile
